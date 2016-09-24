37900bdef3f05b4dab13a53f0829e312813b9e37
Oana Modoranu <oana13@yahoo.com>
2016-09-24 23:25:56 +0300

    Assignment done

---
+++ app.js
@@ -0,0 +1,38 @@
+(function(){
+  'use strict';
+  angular.module('LunchCheck', [])
+
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope){
+    $scope.menu='';
+    $scope.mess='';
+    $scope.messColor='white';
+    $scope.checkMenu = function(){
+      if($scope.menu.length!=0){
+        var arrayOfFood = $scope.menu.split(',');
+        var number = 0;
+        for(var i=0; i<arrayOfFood.length; i++){
+          if(arrayOfFood[i].trim().length!=0) {
+            number++;
+          }
+        }
+        if(number>3){
+          $scope.mess = "Too much!";
+          $scope.messColor='green';
+        }
+        else{
+          $scope.mess = "Enjoy!";
+          $scope.messColor='green';
+        }
+      }
+      else{
+        $scope.mess = "Please enter data first";
+        $scope.messColor='red';
+      }
+    };
+    $scope.sayMessage = function () {
+      return $scope.mess;
+    };
+  };
+})();
